3acf91f5f506188e68e18c0ef12a2e3bb362d2ab
Michał Kuliś <michalkulis1993@gmail.com>
2018-07-31 18:13:57 +0200

    Add components

---
+++ components/App.js
@@ -0,0 +1,40 @@
+class App extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            searchText: '',
+            users: []
+        };
+    }
+    
+    onChangeHandle(event) {
+        this.setState({searchText: event.target.value});
+    }
+    
+    onSubmit(event) {
+        event.preventDefault();
+        const {searchText} = this.state;
+        const url = `https://api.github.com/search/users?q=${searchText}`;
+        fetch(url) 
+            .then(response => response.json())
+            .then(responseJson => this.setState({users: responseJson.items}));
+    }
+    
+    render() {
+        return (
+            <div>
+                <form onSubmit={event => this.onSubmit(event)}>
+                    <label htmlFor="searchText">Search by user name</label>
+                    <input
+                        type="text"
+                        id="seachText"
+                        onChange={event => this.onChangeHandle(event)}
+                        value={this.state.searchText}
+                    />
+                </form>
+                <UserList users={this.state.users}/>
+            </div>
+        );
+    }
+}
---
+++ components/UserList.js
@@ -0,0 +1,14 @@
+class UserList extends React.Component {
+    
+    get users() {
+        return this.props.users.map(user => <User key={user.id} user={user}/>);
+    }
+    
+    render() {
+        return (
+            <div>
+                {this.users}
+            </div>
+        );
+    }
+}
