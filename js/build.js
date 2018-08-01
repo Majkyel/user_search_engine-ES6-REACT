917ce583624e9262daa0f83bbdd06753c6fab70c
Michał Kuliś <michalkulis1993@gmail.com>
2018-08-01 12:05:13 +0200

    Update build.js

--- js/build.js
+++ js/build.js
@@ -1,3 +1,9 @@
 'use strict';
 
-ReactDOM.render(React.createElement(App, null), document.getElementById('root'));
+var _App = require('../components/App');
+
+var _App2 = _interopRequireDefault(_App);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+ReactDOM.render(React.createElement(_App2.default, null), document.getElementById('root'));
